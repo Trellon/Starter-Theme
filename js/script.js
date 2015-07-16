51cc31d9bf40085b12e54d8d0749f4e665e793ed
Michael Haggerty <mhaggerty@trellon.com>
2015-07-16 14:59:29 -0400

    adding support for responsive javascript

--- js/script.js
+++ js/script.js
@@ -17,7 +17,44 @@
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
 
-    // Place your code here.
+    $('body', context).once('ajf', function(){
+      
+      // Selectors. Set them here for anything you plan to target.
+      var headerSelector = 'header#header';
+      
+      // jRespond - gives javascript support for breakpoints. Wrap all functions in a breakpoint unless it's needed on all displays.
+      // The defaults are from Zen. Adjust them for your own use.
+      // This is included because it's easily overlooked. Feel free to delete if your theme does not use javascript.
+      if (typeof jRespond == 'function') { 
+        var jRes = jRespond([
+           {
+             label: 'smartphones', enter: 0, exit: 480
+           },{
+             label: 'tablets', enter: 481, exit: 959
+           },{
+             label: 'desktops', enter: 960, exit: 10000
+           }
+         ]);
+        // example implementation
+        jRes.addFunc({
+          breakpoint: 'desktops',
+          enter: function() {
+            console.log('Being viewed on a desktop!');
+          },
+          exit: function() {
+            console.log('Leaving desktop view.');
+          },
+        });
+      }; // end of jRespond
+      
+      // neuter console when it should not be running
+      if (!DEBUG_MODE_ON) {
+        console = console || {};
+        console.log = function(){};
+      };  // end of neuter console
+      
+    });
+    
 
   }
 };
